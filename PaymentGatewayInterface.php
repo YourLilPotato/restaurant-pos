dba015764cfdf5c8c8b5107d151b77e0f5496edc
Yourlilpotato <humairazannat639@gmail.com>
2026-07-15 01:23:37 +0600

    Refactor PaymentGatewayInterface to include strict types and improve namespace declaration

--- PaymentGatewayInterface.php
+++ PaymentGatewayInterface.php
@@ -1,7 +1,10 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App;
+
 interface PaymentGatewayInterface
 {
-        // ei interface er moddhe ekta function signature/method signature ache
     public function charge(float $amount): bool;
 }
-?>
